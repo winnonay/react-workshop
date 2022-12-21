e67ff3a47322b6a4560e8d609cf4b7e50b45c6a7
winnonay <winnieeli1103@gmail.com>
2022-12-20 21:24:16 -0500

    update icon and date
    update icon and date

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -6,20 +6,9 @@ export default function WeatherSearch(props) {
         <div className="weatherSearch">
     <FormattedDate date={props.data.date} />
     <h1>{props.data.city}</h1>
-   
-   <div className="container">
-    <div className="row">
-        <div className="col-6">
-        <img src={props.data.iconUrl} className="icon"/>
-        </div>
-    <div className="col-6">
-    {props.data.description}
-    </div>
-   </div>
-   </div>
-
 
     <ul>
+        <li>  <img src={props.data.iconUrl} alt={props.data.description} className="float-left"/> </li>
       <li>{Math.round(props.data.temperature)}°C</li>
       <li>Humidity: {props.data.humidity}%</li>
       <li>Wind Speed:{Math.round(props.data.wind)} km/h</li>
