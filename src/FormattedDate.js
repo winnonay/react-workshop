9f8b018d29b14d4f8e135ec911a557ab14f2bd6d
winnonay <winnieeli1103@gmail.com>
2022-12-21 20:07:17 -0500

    Update FormattedDate.js

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -12,5 +12,5 @@ export default function FormattedDate(props) {
     if (minutes < 10) {
         minutes = `0${minutes}`;
     }
-    return <div>{day} {hours}:{minutes}</div>
+    return <div className="date">{day} {hours}:{minutes}</div>
 }
