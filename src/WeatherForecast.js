39d4ce9a9b7da2ba639f05ac67e56c0927e931c7
winnonay <winnieeli1103@gmail.com>
2022-12-28 15:08:25 -0500

    created animated forecast icon
    created animated icon for forecast only

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -7,7 +7,7 @@ export default function WeatherForecast(props) {
         <div className="row">
             <div className="col-2">
                 <div className="Weather-days">Monday</div>
-                <WeatherIcon />
+           <WeatherIcon code="01d" />
                 <div className="Weather-temp">
                     <span className="Weather-tempmax">Max 19°C</span>
                     <span className="Weather-tempmin"> Min 10°C</span>
